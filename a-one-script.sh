979644901f424757ced6377792c85e36a29d874d
Aubrey Zhang <138344000+auzhangLABS@users.noreply.github.com>
2023-08-30 17:43:08 -0400

    Update a-one-script.sh

--- a-one-script.sh
+++ a-one-script.sh
@@ -1,5 +1,5 @@
+#this is a bash script named a-one-script.sh
 #!/bin/bash
-
 #This is a one-liner because I try to do it first within crontab
 # This is also the shell script that I'll will be putting into the crontab.
 #first, this script  does sudo apt update, which try to update all the package in my system. 
@@ -10,6 +10,6 @@ sudo apt update | tail -n 1 > /home/ubuntu/update$(date +"%m.%d.%y").txt
 #when this .sh file is called on by the crontab it will create a new txt file with the date every Friday (set by crontab)
 #within the crontab I'll put 
 #0 23 * * 5 /bin/bash /home/ubuntu/a-one-script.sh
-#or 0 23 * * 4 /home/ubuntu/a-one-script.sh
+#or 0 23 * * 5 /home/ubuntu/a-one-script.sh
 #which would allow it to run every Friday at 11 pm. This would only work if cron status is active and running you can check this by putting in this command:
 # sudo service cron status
